1a2e2eb3cbcc9ed23c0fb16be49a451088eca0d5
muhammadindra09 <99633586+muhammadindra09@users.noreply.github.com>
2022-03-20 22:21:36 +0700

    filebarang

---
+++ filebarang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javakasir;
+
+/**
+ *
+ * @author ACER
+ */
+public class filebarang {
+    int id;
+String namaBarang;
+int stockBarang, hargaBarang, terjual;
+
+    public filebarang(int id, String namaBarang, int stockBarang, int hargaBarang, int terjual) {
+        this.id = id;
+        this.namaBarang = namaBarang;
+        this.stockBarang = stockBarang;
+        this.hargaBarang = hargaBarang;
+        this.terjual = terjual;
+    }
+
+}
+    
+
